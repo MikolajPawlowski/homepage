e5a9a40b2d02a26e719c6dcb063da379f1241962
Mikolaj <Pawlowskiem@gmail.com>
2021-04-29 20:57:07 +0200

    attempted improvements on javascript

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
-console.log("Hello there!");
+{
+    const welcome = () => {
+        console.log("Hello there!");
+    }
 
-let changeColor = document.querySelector(".js-changeColor");
-let header = document.querySelector(".js-header");
+    const onChangeColorClick = () => {
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        header.style.color = "#" + randomColor;
+    };
 
-changeColor.addEventListener("click", () => {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    header.style.color = "#" + randomColor;
-});
+    const onShowHiddenClick = () => {
+        hidden.classList.toggle("section__hidden");
+        showHidden.innerText = showHidden.innerText === hideText ? showText : hideText;
+    };
 
-let showHidden = document.querySelector(".js-showHidden");
-let hidden = document.querySelector(".js-section__hidden");
+    welcome();
 
-let showText = "Zamów";
-let hideText = "Schowaj"
-showHidden.addEventListener("click", () => {
-    hidden.classList.toggle("section__hidden");
-    showHidden.innerText = showHidden.innerText === hideText ? showText : hideText;
-});
+    const changeColor = document.querySelector(".js-changeColor");
+    const header = document.querySelector(".js-header");
+
+    changeColor.addEventListener("click", onChangeColorClick);
+
+    const showHidden = document.querySelector(".js-showHidden");
+    const hidden = document.querySelector(".js-section__hidden");
+
+    const showText = "Zamów";
+    const hideText = "Schowaj"
+    showHidden.addEventListener("click", onShowHiddenClick);
+}
